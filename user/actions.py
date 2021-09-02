83aec85fb4781efc927adb63e4617cfddc218246
pygudev <gubaev.andreb@gmail.com>
2021-09-02 09:44:21 +0500

    реализовал метод create в классе RegistrationUser

--- user/actions.py
+++ user/actions.py
@@ -1,12 +1,17 @@
 from rest_framework.request import Request
 
 from user.serializer import CreateUserSerializer
+from .service import SendConfirmMail
 
 
 class RegistrationUser:
-    def __init__(self, serializer_class: CreateUserSerializer, request: Request):
-        self._serializer = serializer_class
+    def __init__(self, request: Request):
         self._request = request
 
     def create(self):
-        pass
+        serializer = CreateUserSerializer(data=self._request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        SendConfirmMail.send_mail(user)
+
+
