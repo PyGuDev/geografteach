c4149c862423ba436fc82303ce684fea04a45dc6
pygudev <gubaev.andreb@gmail.com>
2021-09-04 21:50:26 +0500

    переделал url создания лайка

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import CategoryView, ArticleListView, ImagesForArticleView, SingleArticleView, AddLikeArticleView, FileListView, FileDownLoadView
+from .views import CategoryView, ArticleListView, ImagesForArticleView, SingleArticleView, \
+    AddLikeArticleView, FileListView, FileDownLoadView
 
 
 urlpatterns = [
     path('blog/', ArticleListView.as_view()),
     path('blog/article/<int:pk>/', SingleArticleView.as_view()),
-    path('blog/addLike', AddLikeArticleView.as_view()),
+    path('blog/article/<int:pk>/add_like/', AddLikeArticleView.as_view()),
     path('blog/category/', CategoryView.as_view()),
     path('blog/images/', ImagesForArticleView.as_view()),
     path('blog/files/', FileListView.as_view()),
