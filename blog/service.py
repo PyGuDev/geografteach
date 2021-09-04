38e116a9c13584e99e132a79f867a95caeb94e5e
pygudev <gubaev.andreb@gmail.com>
2021-09-04 22:13:26 +0500

    убрал лишние импорты

--- blog/service.py
+++ blog/service.py
@@ -1,8 +1,4 @@
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.response import Response
-import math
-
-from blog.models import Article
 
 
 def get_client_ip(request):
