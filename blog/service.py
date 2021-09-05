cc75633126c83d173f1cf24df50bd551e5cbfd1c
pygudev <gubaev.andreb@gmail.com>
2021-09-05 22:28:34 +0500

    Сделал кастомный вывод пагинации

--- blog/service.py
+++ blog/service.py
@@ -1,4 +1,5 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
 def get_client_ip(request):
@@ -13,3 +14,12 @@ def get_client_ip(request):
 class PaginationApp(PageNumberPagination):
     """Пагинация"""
     page_size = 7
+
+    def get_paginated_response(self, data):
+        return Response({
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'count': self.page.paginator.count,
+            'count_page': round(self.page.paginator.count/self.page_size),
+            'results': data
+        })
