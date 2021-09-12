cc9261e9c11261ea1d5152a84b8d0303aecb2cf5
pygudev <gubaev.andreb@gmail.com>
2021-09-12 15:33:32 +0500

    Добавил name к урлам приложения user

--- user/urls.py
+++ user/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import SignUpUserAPIView, ConfirmUserAPIView, SingInAPIView, GetUserAPIView
 
 urlpatterns = [
-    path('register/', SignUpUserAPIView.as_view()),
-    path('confirm/', ConfirmUserAPIView.as_view()),
-    path('singin/', SingInAPIView.as_view()),
-    path('profile/', GetUserAPIView.as_view())
+    path('register/', SignUpUserAPIView.as_view(), name='register'),
+    path('confirm/', ConfirmUserAPIView.as_view(), name='confirm'),
+    path('singin/', SingInAPIView.as_view(), name='sing_in'),
+    path('profile/', GetUserAPIView.as_view(), name='profile')
 ]
