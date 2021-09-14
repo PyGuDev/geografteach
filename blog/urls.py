001675d0001957313a6961e56a0e15c0ae9add6c
pygudev <gubaev.andreb@gmail.com>
2021-09-14 09:13:54 +0500

    убрал лишний url

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from .views import CategoryView, ArticleListView, ImagesForArticleView, SingleArticleView, \
+from .views import CategoryView, ArticleListView, SingleArticleView, \
     AddLikeArticleView, FileListView
 
 
 urlpatterns = [
-    path('blog/', ArticleListView.as_view()),
-    path('blog/article/<int:pk>/', SingleArticleView.as_view()),
-    path('blog/article/<int:pk>/add_like/', AddLikeArticleView.as_view()),
-    path('blog/category/', CategoryView.as_view()),
-    path('blog/images/', ImagesForArticleView.as_view()),
-    path('blog/files/', FileListView.as_view()),
+    path('blog/', ArticleListView.as_view(), name='list_article'),
+    path('blog/article/<int:pk>/', SingleArticleView.as_view(), name='single_article'),
+    path('blog/article/<int:pk>/add_like/', AddLikeArticleView.as_view(), name='add_like'),
+    path('blog/category/', CategoryView.as_view(), name='list_category'),
+    path('blog/files/', FileListView.as_view(), name='list_files'),
 ]
